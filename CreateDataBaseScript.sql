0a08ea44fc89ecb16bada5d625e0b320bcadb736
Rafael <RafaeleSmith@hotmail.com>
2020-04-13 17:42:18 -0400

    Add files via upload
    changed field names to be more consistent

--- CreateDataBaseScript.sql
+++ CreateDataBaseScript.sql
@@ -12,44 +12,44 @@ use COP4710;
 
 #create the schemas for the four relations in this database
 create table items (
-    inum integer auto_increment,
-	iname varchar(30) not null,
-	icost float,
+    iNum integer auto_increment,
+	iName varchar(30) not null,
+	iCost float,
 	iImage varchar(80),
-    index (inum)
+    index (iNum)
 );
 
 create table orders (
-    onum integer auto_increment,
-	discount_id integer,
-	item_id integer not null,
+    oNum integer auto_increment,
+	oDiscount_id integer,
+	oItem_id integer not null,
 	oQuantity integer,
 	oCusName varchar(30) not null,
 	oCusPhone varchar(30),
 	oCusEmail varchar(80),
-    index (onum),
-	foreign key (item_id) references items(inum)
+    index (oNum),
+	foreign key (oItem_id) references items(iNum)
 );
 
 create table formula (
-    fnum integer auto_increment,
-	timeLeft integer,
-	quantityStep integer,
-	discountStep float,
-	maxDiscount float,
-    index (fnum)
+    fNum integer auto_increment,
+	fTimeLeft integer,
+	fQuantityStep integer,
+	fDiscountStep float,
+	fMaxDiscount float,
+    index (fNum)
 );
 
 create table discount (
-    dnum integer auto_increment,
-	item_id integer not null,
-	formula_id integer,
+    dNum integer auto_increment,
+	dItem_id integer not null,
+	dFormula_id integer,
 	dCode varchar(50),
 	dStart integer,
 	dEnd integer,
-    index (dnum),
+    index (dNum),
 	unique key discount_code (dCode),
-	foreign key (item_id) references items(inum)
+	foreign key (dItem_id) references items(iNum)
 );
     
 # populate the database tables
@@ -57,11 +57,11 @@ insert into items (iname, icost, iImage) values ('Pencil', 0.53, 'pencil.jpg' );
 insert into items (iname, icost, iImage) values ('Eraser', 0.73, 'eraser.jpg' );
 insert into items (iname, icost, iImage) values ('Paper', 1.50, 'paper.jpg' );
 
-insert into formula (timeLeft,quantityStep,discountStep, maxDiscount) values ( 10, 5, 8.0, 40.0);
+insert into formula (ftimeLeft,fquantityStep,fdiscountStep, fmaxDiscount) values ( 10, 5, 8.0, 40.0);
 
-insert into discount (item_id, formula_id, dCode, dStart,dEnd) values (1,1, 'dXc17sP', 041320,042020);
+insert into discount (ditem_id, dformula_id, dCode, dStart,dEnd) values (1,1, 'dXc17sP', 041320,042020);
 
-insert into orders (discount_id, item_id, oQuantity, oCusName, oCusPhone, oCusEmail) values (1,1,10, 'PencilMan', '555-5555', 'pencilman@pencils.com');
+insert into orders (odiscount_id, oitem_id, oQuantity, oCusName, oCusPhone, oCusEmail) values (1,1,10, 'PencilMan', '555-5555', 'pencilman@pencils.com');
 select * from items;
 select * from formula;
 select * from discount;
