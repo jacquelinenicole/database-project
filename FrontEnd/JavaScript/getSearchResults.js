86bb2d9cafdc0107d089a1fe7e870245ce592720
Rafaelgit config --global user.name Rafaelgit config --global user.name Rafael <RafaeleSmith@hotmail.com>
2020-04-22 13:16:17 -0400

    Summary
    Extended the Search function to work on the itempage.html
    
    moved scripts on itempage.html to getSearchResults.js

--- FrontEnd/JavaScript/getSearchResults.js
+++ FrontEnd/JavaScript/getSearchResults.js
@@ -18,7 +18,7 @@ function getSearchResults(searchTextId, mainContentPage) {
 					itemimage =info.items[i].image;
 						content += `<div class="card col-md-4" style="width: 18rem;">
 										<img class="card-img-top" src="..." alt="Image name: ${itemimage}">
-											<div class="card-body">
+										<div class="card-body">
 											<p class="card-text"><b>${itemname}</b></p>
 											<p class="card-text">${itemdesc}</p>
 											<p class="card-text">$${itemcost}</p>
@@ -28,7 +28,47 @@ function getSearchResults(searchTextId, mainContentPage) {
 									</div>`;
 				}
 			}
+			content += "</div>";
 			document.getElementById(mainContentPage).innerHTML = content;
+		}
+	}
+	xmlhttp.open("POST", "../../api/get_items.php", true);
+	xmlhttp.setRequestHeader("Content-type", 'application/json; charset=UTF-8');
+	xmlhttp.send();
+}
+
+
+function displayAllProducts(ContentPage)
+{
+	var i, content ='<div class="row" style="margin-top: 10%;">';
+					
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function ()
+	{
+		if(this.readyState == 4 && this.status == 200)
+		{
+			var info = JSON.parse(this.responseText);
+			var itemname, itemdesc, itemcost, itemimage;
+			for(i in info.items)
+			{
+			itemname = info.items[i].name;
+			itemdesc = info.items[i].desc;
+			itemcost = info.items[i].cost;
+			itemimage =info.items[i].image;
+				content += `<div class="card col-md-4" style="width: 18rem;">
+								<img class="card-img-top" src="..." alt="Image name: ${itemimage}">
+								<div class="card-body">
+									<p class="card-text"><b>${itemname}</b></p>
+									<p class="card-text">${itemdesc}</p>
+									<p class="card-text">$${itemcost}</p>
+									<button type="button" class="btn btn-info">Request Discount</button>
+									<button type="button" class="btn btn-success">Add to Cart</button>
+								</div>
+							</div>`;
+			}
+			content += "</div>";
+			document.getElementById(ContentPage).innerHTML = content;
+			
 		}
 	}
 	xmlhttp.open("POST", "../../api/get_items.php", true);
