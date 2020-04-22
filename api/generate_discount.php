d0787a3cc70e18f9537642b38b353805441df314
Rafaelgit config --global user.name Rafaelgit config --global user.name Rafael <RafaeleSmith@hotmail.com>
2020-04-22 19:28:11 -0400

    adad
    Something

---
+++ api/generate_discount.php
@@ -0,0 +1,71 @@
+<?php
+	include("./config/database.php");	
+	$response = new \stdClass();
+	$obj = json_decode(file_get_contents('php://input'), true);
+	$database = new Database();
+	$db = $database->mysqliConnection();
+	//$database->createSession();
+	start_session();
+	
+	$permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	
+	if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		if(mysqli_connect_errno($db))
+		{
+			$repsonse->text = "Error with DB connect";
+		}
+		else
+		{
+			$response->transmit = $obj;
+			
+			//json error catch
+			switch (json_last_error()) 
+			{
+				case JSON_ERROR_NONE:
+					$response->errorlog = ' - No errors';
+				break;
+				case JSON_ERROR_DEPTH:
+					$response->errorlog = ' - Maximum stack depth exceeded';
+				break;
+				case JSON_ERROR_STATE_MISMATCH:
+					$response->errorlog = ' - Underflow or the modes mismatch';
+				break;
+				case JSON_ERROR_CTRL_CHAR:
+					$response->errorlog = ' - Unexpected control character found';
+				break;
+				case JSON_ERROR_SYNTAX:
+					$response->errorlog = ' - Syntax error, malformed JSON';
+				break;
+				case JSON_ERROR_UTF8:
+					$response->errorlog = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+				break;
+				default:
+					$response->errorlog = ' - Unknown error';
+				break;
+			}
+			
+			//get itemId
+			//get today's date
+			$response->code = generate_string($permitted_chars, 5);
+			
+			
+		}
+		echo json_encode($response);
+		exit();
+	}
+	
+
+	function generate_string($input, $strength = 16) 
+	{
+		$input_length = strlen($input);
+		$random_string = '';
+		for($i = 0; $i < $strength; $i++) 
+		{
+			$random_character = $input[random_int(0, $input_length - 1)];
+			$random_string .= $random_character;
+		}
+ 
+		return $random_string;
+	}
+?>
