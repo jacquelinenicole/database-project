40ca1400a2bdda1a6c69ba9cf7443c4ac3d0b416
JacksonTrudel <43221411+JacksonTrudel@users.noreply.github.com>
2020-04-23 02:55:42 -0400

    Add files via upload

--- FrontEnd/JavaScript/getSearchResults.js
+++ FrontEnd/JavaScript/getSearchResults.js
@@ -15,7 +15,7 @@ function getSearchResults(searchTextId, mainContentPage) {
 		if(this.readyState == 4 && this.status == 200)
 		{
 			var info = JSON.parse(this.responseText);
-			var itemname, itemdesc, itemcost, itemimage;
+			var itemname, itemdesc, itemcost, itemimage, itemimagename;
 			for(i in info.items)
 			{
 				if(info.items[i].name.toLowerCase() === document.getElementById(searchTextId).value.toLowerCase()) 
@@ -24,14 +24,15 @@ function getSearchResults(searchTextId, mainContentPage) {
 					itemname = info.items[i].name;
 					itemdesc = info.items[i].desc;
 					itemcost = info.items[i].cost;
-					itemimage =info.items[i].image;
+					itemimage = "images/" + info.items[i].image;
+					itemimagename = info.items[i].image;
 						content += `<div class="card col-md-4" style="width: 18rem;">
-										<img class="card-img-top" src="..." alt="Image name: ${itemimage}">
+										<img class="card-img-top" src="${itemimage}" alt="Image name: ${itemimagename}">
 										<div class="card-body">
 											<p class="card-text"><b>${itemname}</b></p>
 											<p class="card-text">${itemdesc}</p>
 											<p class="card-text">$${itemcost}</p>
-											<button type="button" class="btn btn-info">Request Discount</button>
+											<button type="button" class="btn btn-info" onClick="gen_code(${itemid})">Request Discount</button>
 											<button type="button" class="btn btn-success" onClick="addtoCart(${itemid}, 1)">Add to Cart</button>
 										</div>
 									</div>`;
@@ -57,21 +58,22 @@ function displayAllProducts(ContentPage)
 		if(this.readyState == 4 && this.status == 200)
 		{
 			var info = JSON.parse(this.responseText);
-			var itemname, itemdesc, itemcost, itemimage;
+			var itemname, itemdesc, itemcost, itemimagename, itemimage;
 			for(i in info.items)
             {
                 itemid = info.items[i].id;
 			itemname = info.items[i].name;
 			itemdesc = info.items[i].desc;
 			itemcost = info.items[i].cost;
-			itemimage =info.items[i].image;
+			itemimage = "images/" + info.items[i].image;
+			itemimagename = info.items[i].image;
 				content += `<div class="card col-md-4" style="width: 18rem;">
-								<img class="card-img-top" src="..." alt="Image name: ${itemimage}">
+								<img class="card-img-top" src="${itemimage}" alt="Image name: ${itemimagename}">
 								<div class="card-body">
 									<p class="card-text"><b>${itemname}</b></p>
 									<p class="card-text">${itemdesc}</p>
 									<p class="card-text">$${itemcost}</p>
-									<button type="button" class="btn btn-info">Request Discount</button>
+									<button type="button" class="btn btn-info" onClick="gen_code(${itemid})">Request Discount</button>
 									<button type="button" class="btn btn-success" onclick="addtoCart(${itemid}, 1)">Add to Cart</button>
 								</div>
 							</div>`;
@@ -86,6 +88,29 @@ function displayAllProducts(ContentPage)
 	xmlhttp.send();
 	
 	return;
+}
+
+function gen_code(itemid)
+{
+	console.log("gen_code() " + itemid);
+	var message = `{"itemid" : "${itemid}"}`;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function ()
+	{
+		if(this.readyState == 4 && this.status == 200)
+		{
+			console.log(this.responseText);
+			var info = JSON.parse(this.responseText);
+			
+			if(info.code !== 'undefined')
+			{
+				alert("your code is! " + info.code);
+			}
+		}
+	}
+	xmlhttp.open("POST", "../../api/generate_discount.php", true);
+	xmlhttp.setRequestHeader("Content-type", 'application/json; charset=UTF-8');
+	xmlhttp.send(message);
 }
 
 function addtoCart(itemid, itemquantity)
@@ -105,14 +130,14 @@ function addtoCart(itemid, itemquantity)
 }
 
 function displayShoppingCart(ContentPage) {
-    var content = ``;
+    var i, content = ``;
 
            
-    var message = `{"foo" : "getCart"}`; //, "id" : "${itemid}", "quantity" : "${itemquantity}"}`;
+    var message = `{"foo" : "getCart"}`;
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
-        console.log(this.responseText);
+        //console.log(this.responseText);
         if (this.readyState == 4 && this.status == 200) {
             var info = JSON.parse(this.responseText);
 
@@ -125,19 +150,18 @@ function displayShoppingCart(ContentPage) {
 							</div></div>`;
             }
             else {
-                var itemname, itemdesc, itemcost, itemimage;
+                var itemname, itemdesc, itemcost, itemquant, itemimage, itemimagename;
 
                 var firstItem = true;
 
                 for (i in info.items) {
-                    console.log(i);
                     itemid = info.items[i].id;
                     itemname = info.items[i].name;
                     itemdesc = info.items[i].desc;
                     itemquant = info.items[i].quantity;
-                    itemcost = info.items[i].cost * itemquant;
-                    itemimage = info.items[i].image;
-                    
+                    itemcost = (info.items[i].cost * itemquant);
+                    itemimage = "images/" + info.items[i].image;
+                    itemimagename = info.items[i].image;
                     if (firstItem) {
                         content += `<div class="row justify-content-center" style = "margin-top: 10%;" >`;
                         firstItem = false;
@@ -158,9 +182,11 @@ function displayShoppingCart(ContentPage) {
                                 <div class="card" id="discount" style="width: 18rem;">
                                     <div class="card-body">
                                         <div class="form-group">
-                                            <input type="text" class="form-control" id="discount-field" placeholder="Enter your discount code">
+                                            <input type="text" class="form-control" id="discount-field-${itemid}" placeholder= "Enter your discount code"
+                                            onkeypress="return validateDiscountCode(event, this);"
+    										minlength="5" maxlength="5">
                                         </div>
-                                            <button type="button" class="btn btn-info" onclick="applyDiscount()">Apply discount</button>
+                                            <button type="button" class="btn btn-info" onclick="applyDiscount(${itemid}, document.getElementById('discount-field-${itemid}').value)">Apply discount</button>
                                      </div>
                                 </div></div><br>`;
                     
@@ -177,3 +203,4 @@ function displayShoppingCart(ContentPage) {
 
     return;
 }
+
